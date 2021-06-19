b3bd3bfcdc939a710928b80abcc28ef79360536a
KarenaBradley <85700710+KarenaBradley@users.noreply.github.com>
2021-06-19 12:13:16 -0400

    Add files via upload
    P2HW1

---
+++ P2HW1_DistanceTraveled_KarenaBradley.py
@@ -0,0 +1,12 @@
+# This project is to calculate the total distance traveled
+# 6/18/2001
+# CTI-110 P2HW1 - DistanceTraveled
+# Karena Bradley
+
+>car_speed = int(input("Enter car's speed: 70"))
+time_traveled = int(input("Enter time traveled: 3"))
+
+speed_entered = int(input("Speed entered: 70.0"))
+time_entered = int(input("Time entered: 3.0"))
+
+distance_traveled = int(input("Distance Traveled: 210.0 miles"))
